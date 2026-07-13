eac8d8089de83b7bc8c59c5faad62a3d7a09acfc
rghv3141 <raghav.original@gmail.com>
2026-07-13 15:00:15 +0530

    reading from the file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 int main(int argc, char** argv) {
-		
-		return 0;
+	if (argc != 2) {
+		std::cerr << "Incorrect Usage" << std::endl;
+		std::cerr << "rcc <input.c>" << std::endl;
+		return EXIT_FAILURE;
+	}
+	
+	
+	std::string contents;
+	{
+		std::stringstream contents_stream;
+		std::fstream input(argv[1], std::ios::in);
+		contents_stream << input.rdbuf();
+		contents = contents_stream.str();
+	}
+
+	std::cout << contents << std::endl;
+
+	return EXIT_SUCCESS;
 } 
